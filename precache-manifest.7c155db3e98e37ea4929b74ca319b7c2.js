97f3f9d6ec50b60c2b559d48923be4abfd87ba3e
vaibhavnagre <nagre.vaibhav8@gmail.com>
2020-07-06 03:09:38 +0530

    Updates

---
+++ precache-manifest.7c155db3e98e37ea4929b74ca319b7c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8223d968c48eacbcb8c121b1cb3604a1",
+    "url": "/Google-Keep-Clone/index.html"
+  },
+  {
+    "revision": "c669879a7f9667bb6bb2",
+    "url": "/Google-Keep-Clone/static/js/2.b0d2781b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Google-Keep-Clone/static/js/2.b0d2781b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cebccb01a2907353782",
+    "url": "/Google-Keep-Clone/static/js/main.85bab707.chunk.js"
+  },
+  {
+    "revision": "c9471227219f329fcf5a",
+    "url": "/Google-Keep-Clone/static/js/runtime-main.5367102a.js"
+  }
+]);
